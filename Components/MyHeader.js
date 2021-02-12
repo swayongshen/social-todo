a69f52fcf87d8e805cb6ce868dc9611e58c25544
Swa Yong Shen <yongshenswa@gmail.com>
2021-02-12 17:55:30 +0800

    Integrate add task button into header, delete component file AddTaskButton.js

--- Components/MyHeader.js
+++ Components/MyHeader.js
@@ -1,57 +1,64 @@
 import React, { useState } from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Header } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign';
+import AddIcon from 'react-native-vector-icons/AntDesign';
 
-import AddTaskButton from './AddTaskButton';
 
-const MyHeader = ({addTask}) => {
-  const [showMenu, setShowMenu] = useState(false);
+const MyHeader = ({ addTask }) => {
+    const [showMenu, setShowMenu] = useState(false);
 
-  return (<Header
-    leftComponent={showMenu 
-      ? 
-        <Icon.Button
-          name="menu-unfold"
-          backgroundColor="#121212"
-          color="white"
-          style={style.button}
-          onPress={() => {
-            setShowMenu(() => false);
-          }}/>
-      :
-        <Icon.Button 
-          name="menu-fold"
-          backgroundColor="#121212"
-          color="white"
-          style={style.button}
-          onPress={() => {
-            setShowMenu(() => true)}}/>
-    }
-    centerComponent={ <Text style={style.title}>Social To Do</Text> }
-    rightComponent={ <AddTaskButton addTask={addTask}/>}
-    containerStyle={style.header}
-  />);
+    /** Show either open or fold menu button depending on whether menu is open. */
+    const menuButton = <Icon.Button
+        name={showMenu ? "menu-unfold" : "menu-fold"}
+        backgroundColor="#121212"
+        color="white"
+        style={style.menuButton}
+        onPress={() => {
+            setShowMenu((currState) => !currState);
+    }} />
+
+    const title = <Text style={style.title}>Social To Do</Text>
+
+    const addTaskButton = <AddIcon.Button
+        name="plus"
+        backgroundColor="#121212"
+        color="white"
+        style={style.addButton}
+        onPress={() => addTask()}
+    />
+
+    return (<Header
+        leftComponent={ menuButton }
+        centerComponent={ title }
+        rightComponent={ addTaskButton }
+        containerStyle={style.header}
+    />);
 }
 
 const style = StyleSheet.create({
-  header: {
-    backgroundColor: '#121212',
-    justifyContent: "space-around"
-  },
+    header: {
+        backgroundColor: '#121212',
+        justifyContent: "space-around"
+    },
 
-  title: {
-    color:"white",
-    fontFamily:"sans-serif",
-    fontSize:24,
-    paddingTop:5,
-    paddingBottom:0
-  },
+    title: {
+        color: "white",
+        fontFamily: "sans-serif",
+        fontSize: 24,
+        paddingTop: 5,
+        paddingBottom: 0
+    },
 
-  button: {
-    paddingTop:12,
-    paddingBottom:0
-  }
+    menuButton: {
+        paddingTop: 12,
+        paddingBottom: 0
+    },
+
+    addButton: {
+        paddingTop:14,
+        paddingBottom:0,
+    }
 })
 
 
