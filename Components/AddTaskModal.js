0185f4d9187b0153c89c84c213f107c6046bf25e
Swa Yong Shen <yongshenswa@gmail.com>
2021-02-12 20:41:20 +0800

    Add form to add task modal. Can now add tasks

--- Components/AddTaskModal.js
+++ Components/AddTaskModal.js
@@ -1,20 +1,77 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-
+import React, {useContext} from 'react';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
 import Modal from 'react-native-modal';
-
+import { useForm, Controller } from 'react-hook-form';
+import { ListContext } from '../App'
 
 const AddTaskModal = ({ showAddTask, setShowAddTask }) => {
+    const { control, handleSubmit, errors } = useForm();
+    const state = useContext(ListContext);
+
+    const addTask = (formData) => {
+        const currTasks = state.tasks;
+        const newTask = {id: currTasks.length + 1, isDone: false, description: formData.description}
+        currTasks.push(newTask);
+        state.setTasks(currTasks);
+        setShowAddTask(false);
+    }
+
     return (
         <View>
-          <Modal isVisible={showAddTask}>
-            <View style={{ flex: 1 }}>
-              <Text>I am the modal content!</Text>
-              <Button title="Cancel" onPress={() => setShowAddTask(() => false)}/>
-            </View>
-          </Modal>
+            {console.log("render")}
+            <Modal isVisible={showAddTask} backdropColor="white">
+                <View style={style.form}>
+                    <Controller
+                        control={control}
+                        render={({ onChange, onBlur, value }) => (
+                            <TextInput
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                                placeholder="Enter your new to do here."
+                            />
+                        )}
+                        name="description"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.description && <Text>This is required.</Text>}
+                    <View style={style.buttonRow}>
+                        <Button 
+                            title="Add" 
+                            onPress={handleSubmit(addTask)}
+                        />
+                        <Text style={{backgroundColor:"white"}}>  </Text>
+                        <Button title="Cancel" 
+                            onPress={() => {
+                                setShowAddTask(() => false)}
+                            }
+                        />
+                    </View>
+                </View>
+            </Modal>
         </View>
-      )
+    )
 }
+
+const style = StyleSheet.create({
+    form: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        maxHeight: 90
+    },
+
+    buttonRow: {
+        flex:1,
+        flexDirection:"row",
+        backgroundColor: "black"
+    },
+
+    buttons: {
+        height: 20
+    }
+})
 
 export default AddTaskModal;
